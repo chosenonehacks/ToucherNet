7a18b5db41289934f8bda1a9babdc0027c8394ff
chos3n1 <chos3n1@COMMANDO>
2023-12-29 21:45:14 +0100

    Add project files.

---
+++ ToucherNet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ToucherNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "file.txt";
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    // Create the file if it doesn't exist
+                    File.Create(filePath).Close();
+                }
+
+                // Update the file's last write time
+                File.SetLastWriteTime(filePath, DateTime.Now);
+
+                // Append the current timestamp to the file
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.WriteLine(DateTime.Now);
+                }
+
+                Console.WriteLine("File touched successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
